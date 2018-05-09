ce1fac844d34859349d50962c9666af218c64531
Simon Erkelens <simonerkelens@silverstripe.com>
2018-05-09 20:39:43 +1200

    Email is sent. It should be tested.

--- src/jobs/PartialSubmissionJob.php
+++ src/jobs/PartialSubmissionJob.php
@@ -2,7 +2,10 @@
 
 namespace Firesphere\PartialUserforms\Jobs;
 
+use SilverStripe\Control\Email\Email;
 use SilverStripe\ORM\DataList;
+use SilverStripe\ORM\FieldType\DBDatetime;
+use SilverStripe\SiteConfig\SiteConfig;
 use SilverStripe\UserForms\Model\UserDefinedForm;
 use Symbiote\QueuedJobs\Services\AbstractQueuedJob;
 
@@ -22,10 +25,22 @@ class PartialSubmissionJob extends AbstractQueuedJob
      */
     public function process()
     {
+        $config = SiteConfig::current_site_config();
         /** @var DataList|UserDefinedForm[] $exportForms */
         $exportForms = UserDefinedForm::get()->filter(['ExportPartialSubmissions' => true]);
+        $files = [];
         foreach ($exportForms as $form) {
             // @todo generate a CSV for each form that should be exported;
         }
+
+        /** @var Email $mail */
+        $mail = Email::create();
+
+        $mail->setSubject('Partial form submissions of ' . DBDatetime::now()->Format(DBDatetime::ISO_DATETIME));
+        foreach ($files as $file) {
+            $mail->addAttachment($file);
+        }
+        $mail->setTo($config->SendMailTo);
+        $mail->send();
     }
 }
