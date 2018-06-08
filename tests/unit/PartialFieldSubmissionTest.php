b6801f57198e220adf9125c57b83e703de9d58fe
Simon Erkelens <simonerkelens@silverstripe.com>
2018-06-08 12:24:03 +1200

    Test true and false scenario

--- tests/unit/PartialFieldSubmissionTest.php
+++ tests/unit/PartialFieldSubmissionTest.php
@@ -3,7 +3,11 @@
 namespace Firesphere\PartialUserforms\Tests;
 
 use Firesphere\PartialUserforms\Models\PartialFieldSubmission;
+use Firesphere\PartialUserforms\Models\PartialFormSubmission;
 use SilverStripe\Dev\SapphireTest;
+use SilverStripe\Security\DefaultAdminService;
+use SilverStripe\Security\Security;
+use SilverStripe\UserForms\Model\UserDefinedForm;
 
 class PartialFieldSubmissionTest extends SapphireTest
 {
@@ -12,30 +16,58 @@ class PartialFieldSubmissionTest extends SapphireTest
      * @var PartialFieldSubmission
      */
     protected $field;
-    
+
     public function testCanView()
     {
         $this->assertFalse($this->field->canView());
+        $member = DefaultAdminService::singleton()->findOrCreateAdmin('admin@example.com');
+        Security::setCurrentUser($member);
+
+        $this->assertTrue($this->field->canView($member));
     }
-    
+
     public function testCanCreate()
     {
-        $this->assertTrue($this->field->canCreate());
+        $this->assertFalse($this->field->canCreate());
+
+        $member = DefaultAdminService::singleton()->findOrCreateAdmin('admin@example.com');
+        Security::setCurrentUser($member);
+
+        $this->assertTrue($this->field->canCreate($member));
+
     }
-    
+
     public function testCanEdit()
     {
         $this->assertFalse($this->field->canEdit());
+
+        $member = DefaultAdminService::singleton()->findOrCreateAdmin('admin@example.com');
+        Security::setCurrentUser($member);
+
+        $this->assertTrue($this->field->canEdit($member));
+
     }
-    
+
     public function testCanDelete()
     {
         $this->assertFalse($this->field->canDelete());
+
+        $member = DefaultAdminService::singleton()->findOrCreateAdmin('admin@example.com');
+        Security::setCurrentUser($member);
+
+        $this->assertTrue($this->field->canDelete($member));
+
     }
-    
+
     protected function setUp()
     {
         $this->field = PartialFieldSubmission::create();
+        $partialForm = PartialFormSubmission::create();
+        $udf = UserDefinedForm::create(['Title' => 'Test'])->write();
+        $partialForm->UserDefinedFormID = $udf;
+        $partialFormID = $partialForm->write();
+        $this->field->SubmittedFormID = $partialFormID;
+
         return parent::setUp();
     }
 }
