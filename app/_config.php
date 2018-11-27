e0dccb25c272ca4a669227ab15e61c8a05561b50
Amol Wankhede <amolsw.nz@gmail.com>
2018-11-27 13:01:33 +1300

    update cms version

---
+++ app/_config.php
@@ -0,0 +1,10 @@
+<?php
+
+use SilverStripe\Security\PasswordValidator;
+use SilverStripe\Security\Member;
+
+// remove PasswordValidator for SilverStripe 5.0
+$validator = PasswordValidator::create();
+$validator->setMinLength(8);
+$validator->setHistoricCount(6);
+Member::set_password_validator($validator);
